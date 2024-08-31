68af25bd95cfb9e79e0e646e5f0281a77e2f2a27
Michael Akoko <akokomichael37@gmail.com>
2024-08-31 15:23:08 +0300

    include script jsx

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,21 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
+    return (
+        <div id="projects">
+            <h2>My Projects</h2>
+            <div>
+                {projects.map((project) => (
+                    <ProjectItem
+                        key={project.id} 
+                        name={project.name}
+                        description={project.about}
+                        technologies={project.technologies}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default ProjectList;
